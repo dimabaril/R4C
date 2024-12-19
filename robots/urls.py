4d58103702f1acf04f27d6756f42a67bdc6c81a7
Dmitrii Barilkin <dimabaril@gmail.com>
2024-12-19 04:06:33 +0100

    chore: kill unnecessary pages

--- robots/urls.py
+++ robots/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
 
 from robots.views import (
-    RobotDetailView,
-    RobotListView,
     create_robot,
     robot_production_summary,
 )
 
 urlpatterns = [
-    path("", RobotListView.as_view(), name="list"),
-    path("<int:pk>/", RobotDetailView.as_view(), name="detail"),
     path("create/", create_robot, name="create"),
     path(
         "production-last-week/", robot_production_summary, name="production-last-week"
